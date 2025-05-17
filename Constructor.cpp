f43dfd572654e32f6a51c80e27cdfa8bac785aec
tari2025 <denilestari272@gmail.com>
2025-05-17 16:04:22 +0700

    petugas

--- Constructor.cpp
+++ Constructor.cpp
@@ -98,3 +98,4 @@ void prosesKembali(Buku &b)
     {
         Buku buku1("Belajar matematika", "J.K. Rowling");
         Peminjam peminjam1("alam", "P123");
+        Petugas petugas1("Budi", "T001", "Admin");
