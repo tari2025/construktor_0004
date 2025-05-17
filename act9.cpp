f17bc0e29381479372169664dedbf636f07e89ff
tari2025 <denilestari272@gmail.com>
2025-05-17 16:20:13 +0700

    proses return

--- act9.cpp
+++ act9.cpp
@@ -22,3 +22,5 @@ int main()
     // bukunya.setJudul("Matematika");
     // cout << bukunya.getJudul();
     cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+    return 0;
+}
